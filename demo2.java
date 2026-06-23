2af9ab2bcbd95b5f8fb71a4e57c0004fd1661e8a
Ankitha <ankithanakitha65825@gmail.com>
2026-06-23 13:41:19 +0530

    modified now

--- demo2.java
+++ demo2.java
@@ -1,5 +1,5 @@
 public class demo2 {
     public static void main(String[] args) {
-        System.out.println("sinchana");
+        System.out.println("sinchana adiga");
     }
 }
--- demo3.java
+++ demo3.java
@@ -1,5 +1,5 @@
 public class demo3 {
     public static void main(String[] args) {
-        System.out.println("Ankitha");
+        System.out.println("Ankitha M");
     }
 }
